2fd65181b228e1b8b35134dbfa05dd7db302ce71
Hussein Muhammed <Hussein.Muhammed091203@gmail.com>
2025-02-16 20:34:43 +0300

    Header sticky part implementation

--- amazon-clone-project/src/Components/Header/UpperHeader.jsx
+++ amazon-clone-project/src/Components/Header/UpperHeader.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import styles from "./UpperHeader.module.css";
 import AmazonLogo from "../../assets/image/amazon_PNG11.png";
 import FlagLogo from "../../assets/image/flags_PNG14655.png";
@@ -6,69 +6,69 @@ import { SlLocationPin } from "react-icons/sl";
 import { BsSearch } from "react-icons/bs";
 import { BiCart } from "react-icons/bi";
 import LowerHeader from "./LowerHeader";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import { DataContext } from "../DataProvider/DataProvider";
 
 const Header = () => {
-
-  const [{basket}, dispatch] = useContext(DataContext)
+  const [{ basket }, dispatch] = useContext(DataContext);
   console.log(basket.length);
 
   return (
     <>
-      <div className={styles.header_container}>
-        <div className={styles.logo_container}>
-          <Link to="/">
-            <img src={AmazonLogo} alt="Amazon Logo" />
-          </Link>
-          <div className={styles.delivery}>
-            <span>
-              <SlLocationPin />
-            </span>
-            <div>
-              <p>Deliverd to</p>
-              <span>Ethiopia</span>
+      <section className={styles.fixed}>
+        <div className={styles.header_container}>
+          <div className={styles.logo_container}>
+            <Link to="/">
+              <img src={AmazonLogo} alt="Amazon Logo" />
+            </Link>
+            <div className={styles.delivery}>
+              <span>
+                <SlLocationPin />
+              </span>
+              <div>
+                <p>Deliverd to</p>
+                <span>Ethiopia</span>
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* search bar */}
-        <div className={styles.search}>
-          <select name="" id="">
-            <option value="">All</option>
-          </select>
-          <input type="text" id="" placeholder="search product" />
-          <BsSearch size={25} />
-        </div>
-
-        {/* right side link */}
-        <div className={styles.order_container}>
-          <Link to="#" className={styles.language}>
-            <img src={FlagLogo} alt="flag logo" />
-
+          {/* search bar */}
+          <div className={styles.search}>
             <select name="" id="">
-              <option value="">EN</option>
+              <option value="">All</option>
             </select>
-          </Link>
+            <input type="text" id="" placeholder="search product" />
+            <BsSearch size={25} />
+          </div>
 
-          <Link to="/auth">
-            <p>Hello, Sign In</p>
-            <span>Account & Lists</span>
-          </Link>
+          {/* right side link */}
+          <div className={styles.order_container}>
+            <Link to="#" className={styles.language}>
+              <img src={FlagLogo} alt="flag logo" />
 
-          <Link to="/orders">
-            <p>Returns</p>
-            <span>& Orders</span>
-          </Link>
+              <select name="" id="">
+                <option value="">EN</option>
+              </select>
+            </Link>
 
-          <Link to="/cart" className={styles.cart}>
-            <BiCart size={35} />
-            <span>{basket.length}</span>
-          </Link>
+            <Link to="/auth">
+              <p>Hello, Sign In</p>
+              <span>Account & Lists</span>
+            </Link>
+
+            <Link to="/orders">
+              <p>Returns</p>
+              <span>& Orders</span>
+            </Link>
+
+            <Link to="/cart" className={styles.cart}>
+              <BiCart size={35} />
+              <span>{basket.length}</span>
+            </Link>
+          </div>
         </div>
-      </div>
-
-      <LowerHeader />
+        <LowerHeader />
+      </section>
     </>
   );
 };
