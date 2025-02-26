302b4ff5dfe2bb5224bd7ca018ddc912e4618648
Hussein Muhammed <Hussein.Muhammed091203@gmail.com>
2025-02-26 08:20:52 +0300

    amazon frontend deployment

--- amazon-clone-project/vite.config.js
+++ amazon-clone-project/vite.config.js
@@ -4,11 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Amazon-Clone/",
   server: {
     port: 2017,
-  },
-  base: "/Amazon-clone/",
-  build: {
-    outDir: "dist",
   },
 });
