6244189bf5c180c18a60ac088ae94021d45fcf37
Hussein Muhammed <Hussein.Muhammed091203@gmail.com>
2025-02-26 21:21:53 +0300

    update json file

--- amazon-clone-project/vite.config.js
+++ amazon-clone-project/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Amazon-Clone/",
+  base: '/Amazon-Clone/',
   server: {
     port: 2017,
   },
